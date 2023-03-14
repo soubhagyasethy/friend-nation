af1cf212558da97b04e3c358ed76083b51298b74
soubhagyasethy <ssethy017@gmail.com>
2023-03-14 23:44:06 +0530

    feat: add custom fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { getPosts } from "../api";
+import Home from "../pages/Home";
+
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log("response", response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <h1>FriendNation</h1>
+      <Home />
     </div>
   );
 }
