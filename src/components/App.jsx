a72d81b6cc71fe67fc325ada36d61db9d7cb6ed4
soubhagyasethy <ssethy017@gmail.com>
2023-03-15 01:44:09 +0530

    feat: add Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import { getPosts } from "../api";
-import Home from "../pages/Home";
+import { Home } from "../pages";
+import { Loader } from "./";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
-      console.log("response", response);
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
     };
 
     fetchPosts();
   }, []);
 
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <h1>FriendNation</h1>
-      <Home />
+      <Home posts={posts} />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,55 @@
-const Home = () => {
-  return <h1>Home</h1>;
+import styles from "../styles/home.module.css";
+
+import user from "../images/user.png";
+import heart from "../images/heart.png";
+import comment from "../images/comment.png";
+
+const Home = ({ posts }) => {
+  return (
+    <div className={styles.postsList}>
+      {posts.map((post) => (
+        <div className={styles.postWrapper}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
+              <img src={user} alt="user-pic" />
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
+            </div>
+            <div className={styles.postContent}>{post.conent}</div>
+
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img src={heart} alt="likes-icon" />
+                <span>5</span>
+              </div>
+
+              <div className={styles.postCommentsIcon}>
+                <img src={comment} alt="comments-icon" />
+                <span>2</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              <div className={styles.postCommentsItem}>
+                <div className={styles.postCommentHeader}>
+                  <span className={styles.postCommentAuthor}>Bill</span>
+                  <span className={styles.postCommentTime}>a minute ago</span>
+                  <span className={styles.postCommentLikes}>22</span>
+                </div>
+
+                <div className={styles.postCommentContent}>Random comment</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
